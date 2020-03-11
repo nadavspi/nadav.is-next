513a515890217daa16f0a6dd44507dbed731fbba
Nadav Spiegelman <me@nadav.name>
2020-03-11 16:01:38 -0400

    Add prismic

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
+import { Client } from '../config/prismic';
+import { RichText } from 'prismic-reactjs';
 
-const Home = () => (
+const Home = ({ doc }) => (
   <div className="container">
     <Head>
       <title>Create Next App</title>
@@ -8,43 +10,7 @@ const Home = () => (
     </Head>
 
     <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
+      {RichText.asText(doc.data.content)}
     </main>
 
     <footer>
@@ -199,5 +165,15 @@ const Home = () => (
     `}</style>
   </div>
 )
+
+export async function getStaticProps(context) {
+  const req = context.req
+  const doc = await Client(req).getSingle('home')
+  return {
+    props: {
+      doc
+    }
+  }
+}
 
 export default Home
