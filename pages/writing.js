fe78142e34596e675812ff546516d82f75ef98c9
Nadav Spiegelman <me@nadav.name>
2020-03-12 10:33:51 -0400

    Use posts singleton

--- pages/writing.js
+++ pages/writing.js
@@ -4,31 +4,37 @@ import { Client, linkResolver, hrefResolver } from "../config/prismic";
 import { Date, RichText } from "prismic-reactjs";
 import Link from "next/link";
 
-export default function Writing({ posts }) {
+export default function Writing({ doc, posts }) {
   return (
-    <main>
+    <div className="container">
       <Head>
-        <title>Posts</title>
+        <title>{RichText.asText(doc.data.heading)}</title>
       </Head>
-      {posts.map(post => (
-        <div>
-          <Link as={linkResolver(post)} href={hrefResolver(post)}>
-            <a>{RichText.asText(post.data.heading)}</a>
-          </Link>
-          <time>{post.data.date}</time>
-        </div>
-      ))}
-    </main>
+
+      <main>
+        <RichText render={doc.data.heading} linkResolver={linkResolver} />
+        {posts.map(post => (
+          <div>
+            <Link as={linkResolver(post)} href={hrefResolver(post)}>
+              <a>{RichText.asText(post.data.heading)}</a>
+            </Link>
+            <time>{post.data.date}</time>
+          </div>
+        ))}
+      </main>
+    </div>
   );
 }
 
 export async function getStaticProps({ params, req }) {
+  const doc = await Client(req).getSingle("posts");
   const posts = await Client().query(
     Prismic.Predicates.at("document.type", "post"),
     { orderings: "[my.post.date desc]" }
   );
   return {
     props: {
+      doc,
       posts: posts.results
     }
   };
