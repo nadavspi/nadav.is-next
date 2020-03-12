83a3d0791952414a5374396c6bb305276ba0c4e6
Nadav Spiegelman <me@nadav.name>
2020-03-12 18:20:31 -0400

    Add FF Tundra

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import App from "next/app";
 import React from "react";
-import { ThemeProvider } from "styled-components";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 const theme = {
   colors: {
@@ -8,11 +8,41 @@ const theme = {
   }
 };
 
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: "FF Tundra";
+    font-weight: bold;
+    src: url("/static/fonts/1468515/c7cb398d-54e6-42ed-9041-e3e1ac428fb0.woff2")
+        format("woff2"),
+      url("/static/fonts/1468515/f30abd5f-0012-4378-923c-b16106dcd3a5.woff")
+        format("woff");
+  }
+  @font-face {
+    font-family: "FF Tundra";
+    font-style: italic;
+    src: url("/static/fonts/1468539/c65ef4d4-020f-4e58-bf9c-f9e5495344f7.woff2")
+        format("woff2"),
+      url("/static/fonts/1468539/14437e62-00cf-40af-be95-739d3404092c.woff")
+        format("woff");
+  }
+  @font-face {
+    font-family: "FF Tundra";
+    src: url("/static/fonts/1468559/79a3e616-8931-4235-b8dc-0f9718329786.woff2")
+        format("woff2"),
+      url("/static/fonts/1468559/98d8ab99-1bd9-4469-8ed9-e5bb30485dce.woff")
+        format("woff");
+  }
+  body {
+    font-family: "FF Tundra";
+  }
+`;
+
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     );
