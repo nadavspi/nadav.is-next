8918288db7175f536c7789f1bca21838458b6fd1
Nadav Spiegelman <me@nadav.name>
2020-06-20 17:35:48 -0400

    Add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import App from "next/app";
-import GlobalStyle from "../components/GlobalStyles";
 import Fonts from "../components/Fonts";
+import GlobalStyle from "../components/GlobalStyles";
+import Head from "next/head";
 import React from "react";
 import theme from "../config/theme";
 import { ThemeProvider } from "styled-components";
@@ -10,6 +11,26 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props;
     return (
       <ThemeProvider theme={theme}>
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
         <Fonts />
         <GlobalStyle />
         <Component {...pageProps} />
