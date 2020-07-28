f67f0fb86bd4710c7e1338423b919c6a705979b0
Nadav Spiegelman <me@nadav.name>
2020-07-28 16:28:20 -0400

    Add margins

--- components/ImageCarousel.js
+++ components/ImageCarousel.js
@@ -1,5 +1,10 @@
 import React from "react";
 import Carousel from "react-image-gallery";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin-top: ${({ theme }) => theme.fontSizes[8]};
+`;
 
 const ImageCarousel = ({ items }) => {
   const images = items.map((item) => {
@@ -9,7 +14,11 @@ const ImageCarousel = ({ items }) => {
       thumbnail: image.thumbnail.url,
     };
   });
-  return <Carousel items={images} />;
+  return (
+    <Container>
+      <Carousel items={images} />
+    </Container>
+  );
 };
 
 export default ImageCarousel;
