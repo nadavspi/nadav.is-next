a9c95b6150ead2a835e86ac77b6e7348036fdc45
Nadav Spiegelman <me@nadav.name>
2021-03-09 12:28:48 -0500

    Add active style for SectionNav

--- components/SectionNav.js
+++ components/SectionNav.js
@@ -3,20 +3,32 @@ import React from "react";
 import styled from "styled-components";
 import { RichText } from "prismic-reactjs";
 import { linkResolver } from "../config/prismic";
+import { useRouter } from "next/router";
 
 const SectionLink = styled.div`
   display: inline-block;
   margin-right: ${({ theme }) => theme.fontSizes[2]};
+
+  a {
+    color: ${(props) => (props.active ? props.theme.colors.cyan : "")};
+
+    &:hover,
+    &:active {
+      color: ${(props) => (props.active ? props.theme.colors.mutedorange : "")};
+    }
+  }
 `;
 
 const SectionNav = ({ items }) => {
   if (!items.length) {
     return null;
   }
+  const router = useRouter();
+  const { section } = router.query;
 
   return items.map((item) => {
     return (
-      <SectionLink>
+      <SectionLink active={section === item.section_id}>
         <Link
           href={{
             query: { section: item.section_id },
