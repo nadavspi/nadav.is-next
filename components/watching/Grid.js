ed6f8d8e9ea0be7caf38628c2c7250297e7c83a0
Nadav Spiegelman <me@nadav.name>
2023-08-30 13:14:16 -0400

    Tweak grid

--- components/watching/Grid.js
+++ components/watching/Grid.js
@@ -5,10 +5,10 @@ import { mq } from "../../config/theme";
 const Container = styled.div`
   display: grid;
   grid-gap: 1rem;
-  max-width: 100%;
+  grid-template-columns: 1fr;
 
   @media (min-width: ${(props) => mq(props, "large")}) {
-    grid-template-columns: 50% 50%;
+    grid-template-columns: repeat(2, 1fr);
   }
 `;
 
