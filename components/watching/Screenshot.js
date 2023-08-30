5fe11b0fca86ab436d1f4b3454b76cf7c8dbe0d0
Nadav Spiegelman <me@nadav.name>
2023-08-30 13:24:50 -0400

    Add sizes attr for screenshots

--- components/watching/Screenshot.js
+++ components/watching/Screenshot.js
@@ -1,8 +1,23 @@
 import React from "react";
 import Image from "next/image";
+import theme from "../../config/theme";
 
-const Screenshot = ({ alt, src, width = 1920, height = 1080 }) => {
-  return <Image src={`https://nadav-website.s3.us-west-001.backblazeb2.com/${src}`} alt={alt} width={width} height={height} />;
+const Screenshot = ({
+  alt,
+  height = 1080,
+  sizes = `(min-width: ${theme.breakpoints.large}) 50vw, 100vw`,
+  src,
+  width = 1920,
+}) => {
+  return (
+    <Image
+      alt={alt}
+      height={height}
+      sizes={sizes}
+      src={`https://nadav-website.s3.us-west-001.backblazeb2.com/${src}`}
+      width={width}
+    />
+  );
 };
 
 export default Screenshot;
