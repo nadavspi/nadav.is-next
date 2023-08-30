f7e81f636586b9367bf10313c7c031d1ce404e99
Nadav Spiegelman <me@nadav.name>
2023-08-30 12:45:10 -0400

    Add Dearest

--- components/YouTube.js
+++ components/YouTube.js
@@ -2,6 +2,6 @@ import React from "react";
 import Embed from "react-lite-youtube-embed";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
-const YouTube = ({ id, title }) => <Embed id={id} title={title} />;
+const YouTube = (props) => <Embed {...props} />;
 
 export default YouTube;
