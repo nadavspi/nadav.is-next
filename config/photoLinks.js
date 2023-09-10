43c767e4241e8571069b38af313ffc93b1793871
Nadav Spiegelman <me@nadav.name>
2023-09-10 15:18:39 -0400

    Change photo nav order

--- config/photoLinks.js
+++ config/photoLinks.js
@@ -1,7 +1,7 @@
 const photoLinks = [
+  { text: "Wyoming", href: "/photography/wyoming" },
   { text: "Andy Miller", href: "/photography/andy-miller" },
   { text: "Eric Goldberg", href: "/photography/eric-goldberg" },
-  { text: "Wyoming", href: "/photography/wyoming" },
 ];
 
 module.exports = { photoLinks };
