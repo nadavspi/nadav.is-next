4d58787c93aa8a787aca151df18380498248e403
Nadav Spiegelman <me@nadav.name>
2024-02-07 11:57:23 -0500

    Add nav and container to app layout

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,17 @@
-import StylingWrapper from "./styling-wrapper"
+import StylingWrapper from "./styling-wrapper";
+import PageContainer from "../components/PageContainer.js";
+import Navigation from "../components/Navigation.js";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <StylingWrapper>{children}</StylingWrapper>
+        <StylingWrapper>
+          <PageContainer>
+            <Navigation />
+            {children}
+          </PageContainer>
+        </StylingWrapper>
       </body>
     </html>
   );
